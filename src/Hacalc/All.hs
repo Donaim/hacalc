204dc677191c098730f13b6c80d51ccafe659be8
Donaim <3Donaim@gmail.com>
2019-08-02 23:47:53 +0200

    create All module

---
+++ src/Hacalc/All.hs
@@ -0,0 +1,14 @@
+
+module Hacalc.All
+	( module Hacalc.Builtins
+	, module Hacalc.Parser
+	, module Hacalc.Run
+	, module Hacalc.Types
+	, module Hacalc.Util
+	) where
+
+import Hacalc.Builtins
+import Hacalc.Parser
+import Hacalc.Run
+import Hacalc.Types
+import Hacalc.Util
