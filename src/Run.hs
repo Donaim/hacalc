6feba55d5c58588260ccf0aa688f793b396e7505
Donaim <3Donaim@gmail.com>
2019-08-02 21:24:48 +0200

    add interpretOneTree

--- src/Run.hs
+++ src/Run.hs
@@ -2,8 +2,10 @@
 module Run where
 
 import PatternT.Types
+import PatternT.SimplifyInterface
 import Types
 import Builtins
+import Util
 
 simplifyCtxInitial :: SimplifyCtx
 simplifyCtxInitial = ()
@@ -26,3 +28,15 @@ pureRules =
 
 mixedRules :: [[SimplifyPattern]] -> [[SimlifyFT]]
 mixedRules patterns = map (\ ps -> map Tuple32 pureRules ++ map Tuple30 ps) patterns
+
+interpretOneTree :: Tree -> [[SimlifyFT]] -> SimplifyMonad [(Tree, Either SimplifyPattern String, SimplifyCtx)]
+interpretOneTree = loop
+	where
+	loop tree [] = return []
+	loop tree (ruleset : rest) = do
+		history <- mixedApplySimplificationsWithPureUntil0Debug ruleset simplifyCtxInitial tree
+		let newtree = if null history
+			then tree
+			else fst3 (last history)
+		next <- loop newtree rest
+		return (history ++ next)
