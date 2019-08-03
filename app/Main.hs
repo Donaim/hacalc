a4b7c3de490b258688535d604de44411f423c393
Donaim <3Donaim@gmail.com>
2019-08-03 09:15:55 +0200

    add --original flag

--- app/Main.hs
+++ app/Main.hs
@@ -17,6 +17,7 @@ data ArgsT
 	= Load
 	{ rulefile :: String
 	, exprfile :: String
+	, original :: Bool
 	}
 	deriving (Show, Data, Typeable)
 
@@ -24,13 +25,14 @@ load :: ArgsT
 load = Load
 	{ rulefile = def &= argPos 0 &= typ "rulefile"
 	, exprfile = def &= argPos 1 &= typ "exprfile"
+	, original = def &= help "Display the original expression in output. Looks like 'expr -> reduced'"
 	}
 
 putErrLn :: String -> IO ()
 putErrLn = hPutStrLn stderr
 
-withText :: String -> String -> IO ()
-withText ruleText exprText =
+withText :: Bool -> String -> String -> IO ()
+withText originalQ ruleText exprText =
 	case interpretRulesAndText ruleText exprText of
 		Left e -> putStrLn $ "Rules have syntax errors: " ++ show e
 		Right results -> mapM_ mapf results
@@ -41,7 +43,8 @@ withText ruleText exprText =
 		Right ok -> do
 			r <- ok
 			let showed = case r of [] -> line ; xs -> stringifyTree (fst3 (last xs))
-			putStrLn $ line ++ " -> " ++ showed
+			let answer = if originalQ then line ++ " -> " ++ showed else showed
+			putStrLn $ answer
 
 
 main :: IO ()
@@ -52,5 +55,5 @@ main = do
 
 	ruleText <- readFile (rulefile args)
 	exprText <- readFile (exprfile args)
-	withText ruleText exprText
+	withText (original args) ruleText exprText
 
