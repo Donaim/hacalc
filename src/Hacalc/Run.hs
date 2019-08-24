73d6bc737e5615a360a8786c7b1266d79c922252
Donaim <3Donaim@gmail.com>
2019-08-24 16:20:13 +0200

    introduce hacalcRun and RunOptions

--- src/Hacalc/Run.hs
+++ src/Hacalc/Run.hs
@@ -1,12 +1,11 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 
 module Hacalc.Run where
 
 import Data.Either
+import Data.Data
 
-import PatternT.Types
-import PatternT.SimplifyInterface
-import PatternT.Parsing
-import PatternT.Display
+import PatternT.All
 import Hacalc.Parser
 import Hacalc.Types
 import Hacalc.Builtins
@@ -35,6 +34,60 @@ delimitingSymbols = ["+", "-", "*", "/", "^"]
 
 mixedRules :: (Monad m) => Rulesets -> [[SimplificationF m ctx]]
 mixedRules patterns = map (\ ps -> map Right3 pureRules ++ map Left3 ps) patterns
+
+data RunOptions = RunOptions
+	{ parseDelimiters                    :: [String]
+	, parseDelimiterPreserveQuotesQ      :: Bool
+	, parseSplitByNumbersQ               :: Bool
+	, parseEnableCommentsQ               :: Bool
+	, displayConcatByNumbersQ            :: Bool
+	, interpretStepLimit                 :: Maybe Int
+	, interpretTreeSizeLimit             :: Maybe Int
+	, interpretCondRecursionLimit        :: Maybe Int
+	} deriving (Eq, Show, Read, Typeable, Data)
+
+hacalcRun :: (Monad m) => RunOptions -> [[SimplificationF m ctx]] -> ctx -> String -> Either ParseError (m (Stdout ctx, Stdout ctx))
+hacalcRun options rules ctx line = either
+	Left
+	(Right . withTokens)
+	(tokenize delimited)
+	where
+	uncommented =
+		if   parseEnableCommentsQ options
+		then fst3 $ partitionString "//" line
+		else line
+	delimiterMode =
+		if   parseDelimiterPreserveQuotesQ options
+		then DelimiterPreserveQuotes
+		else DelimiterIgnoreQuotes
+	delimited =
+		if   null $ parseDelimiters options
+		then uncommented
+		else delimitSymbols delimiterMode (parseDelimiters options) uncommented
+
+	withTokens tokens = do
+		result <- loop (makeTree (Group tokens)) rules
+		return (applyLimits result)
+
+	applyLimits hist = (sizes, dropedSizes)
+		where
+		(steps, dropedSteps) = maybe
+			(hist, [])
+			(\ lim -> splitAt lim hist)
+			(interpretStepLimit options)
+		(sizes, dropedSizes) = maybe
+			(steps, [])
+			(\ lim -> historyLimitTreeSize lim hist)
+			(interpretTreeSizeLimit options)
+
+	loop tree [] = return []
+	loop tree (ruleset : rest) = do
+		history <- mixedApplySimplificationsUntil0Debug (interpretCondRecursionLimit options) ruleset ctx tree
+		let newtree = if null history
+			then tree
+			else fst3 (last history)
+		next <- loop newtree rest
+		return (history ++ next)
 
 interpretOneTree :: (Monad m) => ctx -> [[SimplificationF m ctx]] -> Tree -> m (Stdout ctx)
 interpretOneTree ctx rules t = loop t rules
