406244aa64aa6042d90b18013420d5336dbc7d1f
Donaim <3Donaim@gmail.com>
2020-02-13 14:17:31 +0100

    export CommonFrontend by default

--- src/Hacalc/All.hs
+++ src/Hacalc/All.hs
@@ -4,9 +4,11 @@ module Hacalc.All
 	, module Hacalc.Run
 	, module Hacalc.Types
 	, module Hacalc.UtilExternal
+	, module PatterntCommonFrontend.All
 	) where
 
 import Hacalc.Builtins
 import Hacalc.Run
 import Hacalc.Types
 import Hacalc.UtilExternal
+import PatterntCommonFrontend.All
